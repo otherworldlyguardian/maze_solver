487ec1172ba4fb47c25486e53300fd322121dde0
otherworldlyguardian <otherworldlyguardian@gmail.com>
2025-04-07 14:01:30 -0400

    Maze initialization and cell draw animation

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Tk, BOTH, Canvas
 
 class Window():
@@ -76,3 +77,36 @@ class Cell():
             self._win.draw_line(line, "gray")
         else:
             self._win.draw_line(line, "red")
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            row_builder = [Cell(self._win) for j in range(self._num_rows)]
+            self._cells.append(row_builder)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+
+    def _draw_cell(self, i, j):
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.02)
